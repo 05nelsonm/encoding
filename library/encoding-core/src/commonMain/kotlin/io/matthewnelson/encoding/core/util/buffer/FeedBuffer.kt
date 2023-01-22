4f7005e6f57519dbcfab95b154e272e172a1d6e0
Matthew Nelson <developer@matthewnelson.io>
2023-01-22 06:47:06 -0500

    Finally land on a solid encoding/decoding input buffer implementation (YAY)

---
+++ library/encoding-core/src/commonMain/kotlin/io/matthewnelson/encoding/core/util/buffer/FeedBuffer.kt
@@ -0,0 +1,125 @@
+/*
+ * Copyright (c) 2023 Matthew Nelson
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ **/
+package io.matthewnelson.encoding.core.util.buffer
+
+import io.matthewnelson.encoding.core.Decoder
+import io.matthewnelson.encoding.core.Encoder
+import io.matthewnelson.encoding.core.EncodingException
+import io.matthewnelson.encoding.core.OutFeed
+import io.matthewnelson.encoding.core.internal.Internal
+import io.matthewnelson.encoding.core.internal.InternalEncodingApi
+import kotlin.jvm.JvmField
+import kotlin.jvm.JvmName
+import kotlin.jvm.JvmStatic
+
+/**
+ * Helper class for [Decoder.Feed] and [Encoder.Feed]s
+ * to buffer their input until they are ready to output
+ * data to their [OutFeed]s.
+ *
+ * @see [DecodingBuffer]
+ * @see [EncodingBuffer]
+ * @see [Flush]
+ * @see [Finalize]
+ * @see [truncatedInputEncodingException]
+ * @throws [IllegalArgumentException] if [blockSize] is less
+ *   than or equal to 0
+ * */
+@OptIn(InternalEncodingApi::class)
+public sealed class FeedBuffer<T: Number>
+@Throws(IllegalArgumentException::class)
+constructor(
+    @JvmField
+    public val blockSize: Int,
+    private val flush: Flush<T>,
+    private val finalize: Finalize<T>,
+) {
+
+    init {
+        require(blockSize > 0) {
+            "blockSize must be greater than 0"
+        }
+    }
+
+    @get:JvmName("count")
+    public var count: Int = 0
+        private set
+
+    /**
+     * Update the [buffer] with new input.
+     *
+     * Will automatically invoke [flush] in order
+     * to output data once the [buffer] fills.
+     * */
+    public fun update(input: T) {
+        val buffer = buffer(Internal.get())
+        buffer[count] = input
+
+        if (++count % blockSize == 0) {
+            flush.invoke(buffer)
+            count = 0
+        }
+    }
+
+    /**
+     * Call whenever [Encoder.Feed.doFinalProtected] or
+     * [Decoder.Feed.doFinalProtected] is invoked to
+     * process the remaining input in the [buffer].
+     * */
+    public fun finalize() {
+        val buffer = buffer(Internal.get())
+        buffer.fill(zero(), count)
+        finalize.invoke(count % blockSize, buffer)
+        buffer.fill(zero(), 0, count)
+        count = 0
+    }
+
+    /**
+     * [Flush.invoke] will be called once the [buffer]
+     * fills up, and pass it along to perform bitwise
+     * operations before outputting data.
+     *
+     * @see [update]
+     * */
+    public fun interface Flush<T: Number> {
+        public fun invoke(buffer: Array<T>)
+    }
+
+    /**
+     * [Finalize.invoke] will be called whenever [finalize]
+     * is called to process remaining data in the [buffer]
+     *
+     * @see [finalize]
+     * */
+    public fun interface Finalize<T: Number> {
+        public fun invoke(modulus: Int, buffer: Array<T>)
+    }
+
+    public companion object {
+
+        /**
+         * Helper for generating a standard [EncodingException] when
+         * an illegal modulus is encountered while decoding.
+         * */
+        @JvmStatic
+        public fun truncatedInputEncodingException(modulus: Int): EncodingException {
+            return EncodingException("Truncate input. Illegal Modulus[$modulus]")
+        }
+    }
+
+    protected abstract fun buffer(internal: Internal): Array<T>
+    protected abstract fun zero(): T
+}
