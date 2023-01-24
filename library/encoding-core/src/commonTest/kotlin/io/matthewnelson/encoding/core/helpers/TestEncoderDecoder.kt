f5942a08d3c4e28c82c434ef5d2f0fcd8a02b076
Matthew Nelson <developer@matthewnelson.io>
2023-01-24 05:44:27 -0500

    Add EncoderDecoder.Feed unit tests

---
+++ library/encoding-core/src/commonTest/kotlin/io/matthewnelson/encoding/core/helpers/TestEncoderDecoder.kt
@@ -0,0 +1,39 @@
+/*
+ * Copyright (c) 2023 Matthew Nelson
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ **/
+package io.matthewnelson.encoding.core.helpers
+
+import io.matthewnelson.encoding.core.*
+
+@OptIn(ExperimentalEncodingApi::class)
+class TestEncoderDecoder(config: TestConfig): EncoderDecoder(config) {
+    override fun name(): String = "Test"
+
+    @ExperimentalEncodingApi
+    override fun newEncoderFeed(out: OutFeed): Encoder.Feed {
+        return object : Encoder.Feed() {
+            override fun consumeProtected(input: Byte) { out.output(Byte.MAX_VALUE) }
+            override fun doFinalProtected() { out.output(Byte.MIN_VALUE) }
+        }
+    }
+
+    @ExperimentalEncodingApi
+    override fun newDecoderFeed(out: OutFeed): Decoder.Feed {
+        return object : Decoder.Feed() {
+            override fun consumeProtected(input: Byte) { out.output(Byte.MAX_VALUE) }
+            override fun doFinalProtected() { out.output(Byte.MIN_VALUE) }
+        }
+    }
+}
