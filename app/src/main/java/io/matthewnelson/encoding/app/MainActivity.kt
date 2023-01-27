cc4d381d3b5ee29b3c2830fc06fbe63bd954b254
Matthew Nelson <developer@matthewnelson.io>
2023-01-27 08:51:34 -0500

    Change project name from `component-encoding` -> `encoding` (#86)

--- app/src/main/java/io/matthewnelson/encoding/app/MainActivity.kt
+++ app/src/main/java/io/matthewnelson/encoding/app/MainActivity.kt
@@ -1,27 +1,28 @@
 /*
-*  Copyright 2021 Matthew Nelson
-*
-*  Licensed under the Apache License, Version 2.0 (the "License");
-*  you may not use this file except in compliance with the License.
-*  You may obtain a copy of the License at
-*
-*      http://www.apache.org/licenses/LICENSE-2.0
-*
-*  Unless required by applicable law or agreed to in writing, software
-*  distributed under the License is distributed on an "AS IS" BASIS,
-*  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-*  See the License for the specific language governing permissions and
-*  limitations under the License.
-* */
+ * Copyright (c) 2023 Matthew Nelson
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ **/
 @file:Suppress("SpellCheckingInspection")
 
-package io.matthewnelson.component.encoding.app
+package io.matthewnelson.encoding.app
 
 import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import by.kirich1409.viewbindingdelegate.viewBinding
-import io.matthewnelson.component.encoding.app.databinding.ActivityMainBinding
+import io.matthewnelson.encoding.app.R
+import io.matthewnelson.encoding.app.databinding.ActivityMainBinding
 import io.matthewnelson.encoding.builders.*
 import io.matthewnelson.encoding.core.Encoder.Companion.encodeToString
 import io.matthewnelson.encoding.core.ExperimentalEncodingApi
