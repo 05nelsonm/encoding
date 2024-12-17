1cf83cc168b9d0c68c3ab49dc163a8899ca44fc1
Matthew Nelson <developer@matthewnelson.io>
2024-12-17 11:04:14 -0500

    Fix up encoding benchmarks to not use loops

--- benchmarks/src/commonMain/kotlin/io/matthewnelson/encoding/benchmarks/EncoderDecoderOpts.kt
+++ benchmarks/src/commonMain/kotlin/io/matthewnelson/encoding/benchmarks/EncoderDecoderOpts.kt
@@ -16,63 +16,43 @@
 package io.matthewnelson.encoding.benchmarks
 
 import io.matthewnelson.encoding.base16.Base16
-import io.matthewnelson.encoding.base32.Base32
 import io.matthewnelson.encoding.base32.Base32Crockford
 import io.matthewnelson.encoding.base32.Base32Default
 import io.matthewnelson.encoding.base32.Base32Hex
 import io.matthewnelson.encoding.base64.Base64
 import io.matthewnelson.encoding.core.Decoder
-import io.matthewnelson.encoding.core.Decoder.Companion.decodeToByteArray
 import io.matthewnelson.encoding.core.Encoder
 import io.matthewnelson.encoding.core.EncoderDecoder
 import kotlinx.benchmark.*
 
 abstract class EncoderDecoderBenchmarkBase {
 
-    // "<Characters>:<isConstantTime>"
+    // "<Char>:<Byte>:<isConstantTime>"
     abstract var params: String
     protected abstract fun encoder(isConstantTime: Boolean): EncoderDecoder<*>
 
-    private var bytes = ByteArray(0)
-    private var chars = "_"
+    private var byte: Byte = 0
+    private var char = '_'
     private var feedDecoder: Decoder<*>.Feed = Base16.newDecoderFeed {}.apply { close() }
     private var feedEncoder: Encoder<*>.Feed = Base16.newEncoderFeed {}.apply { close() }
 
     @Setup
     fun setup() {
-        val (chars, isConstantTime) = params.split(':').let {
-            it[0] to (it[1] == TIME_CONST)
-        }
-        val encoder = encoder(isConstantTime)
-
-        val (cLength, bSize) = when (encoder) {
-            is Base16 -> 2 to 1
-            is Base32<*> -> 8 to 5
-            is Base64 -> 4 to 3
-            else -> error("Unknown encoder >> $encoder")
+        val encoder = params.split(':').let { params ->
+            char = params[0][0]
+            byte = params[1].toByte()
+            encoder(params[2] == TIME_CONST)
         }
 
-        this.bytes = chars.decodeToByteArray(encoder)
-        this.chars = chars
-        require(this.bytes.size == bSize) {
-            "bytes.size[${this.bytes.size}] did not match expected size[$bSize] for $encoder"
-        }
-        require(this.chars.length == cLength) {
-            "chars.length[${this.chars.length}] did not match expected length[$cLength] for $encoder"
-        }
         feedDecoder = encoder.newDecoderFeed {}
         feedEncoder = encoder.newEncoderFeed {}
     }
 
     @Benchmark
-    fun decode() {
-        chars.forEach { c -> feedDecoder.consume(c) }
-    }
+    fun decode() { feedDecoder.consume(char) }
 
     @Benchmark
-    fun encode() {
-        bytes.forEach { b -> feedEncoder.consume(b) }
-    }
+    fun encode() { feedEncoder.consume(byte) }
 }
 
 @State(Scope.Benchmark)
@@ -82,10 +62,10 @@ abstract class EncoderDecoderBenchmarkBase {
 @Measurement(iterations = ENC_ITERATIONS_MEASURE, time = ENC_TIME_MEASURE)
 open class Base16Benchmark: EncoderDecoderBenchmarkBase() {
     // CHARS: 0123456789ABCDEF
-    @Param("0A:$TIME_QUICK", "E8:$TIME_QUICK", "0A:$TIME_CONST", "E8:$TIME_CONST")
-    override var params: String = "<Characters>:<isConstantTime>"
+    @Param("3:0:$TIME_CONST", "d:122:$TIME_CONST")
+    override var params: String = "<Char>:<Byte>:<isConstantTime>"
     override fun encoder(isConstantTime: Boolean): EncoderDecoder<*> {
-        return Base16 { this.isConstantTime = isConstantTime }
+        return Base16()
     }
 }
 
@@ -96,8 +76,8 @@ open class Base16Benchmark: EncoderDecoderBenchmarkBase() {
 @Measurement(iterations = ENC_ITERATIONS_MEASURE, time = ENC_TIME_MEASURE)
 open class Base32CrockfordBenchmark: EncoderDecoderBenchmarkBase() {
     // CHARS: 0123456789ABCDEFGHJKMNPQRSTVWXYZ
-    @Param("0AC3DFJ7:$TIME_QUICK", "T9WYR8SZ:$TIME_QUICK", "0AC3DFJ7:$TIME_CONST", "T9WYR8SZ:$TIME_CONST")
-    override var params: String = "<Characters>:<isConstantTime>"
+    @Param("3:-6:$TIME_QUICK", "x:115:$TIME_QUICK", "3:-6:$TIME_CONST", "x:115:$TIME_CONST")
+    override var params: String = "<Char>:<Byte>:<isConstantTime>"
     override fun encoder(isConstantTime: Boolean): EncoderDecoder<*> {
         return Base32Crockford { this.isConstantTime = isConstantTime }
     }
@@ -110,8 +90,8 @@ open class Base32CrockfordBenchmark: EncoderDecoderBenchmarkBase() {
 @Measurement(iterations = ENC_ITERATIONS_MEASURE, time = ENC_TIME_MEASURE)
 open class Base32DefaultBenchmark: EncoderDecoderBenchmarkBase() {
     // CHARS: ABCDEFGHIJKLMNOPQRSTUVWXYZ234567
-    @Param("CA2EF3CB:$TIME_QUICK", "WSY2V4ZZ:$TIME_QUICK", "CA2EF3CB:$TIME_CONST", "WSY2V4ZZ:$TIME_CONST")
-    override var params: String = "<Characters>:<isConstantTime>"
+    @Param("C:-123:$TIME_QUICK", "w:15:$TIME_QUICK", "C:-123:$TIME_CONST", "w:15:$TIME_CONST")
+    override var params: String = "<Char>:<Byte>:<isConstantTime>"
     override fun encoder(isConstantTime: Boolean): EncoderDecoder<*> {
         return Base32Default { this.isConstantTime = isConstantTime }
     }
@@ -124,8 +104,8 @@ open class Base32DefaultBenchmark: EncoderDecoderBenchmarkBase() {
 @Measurement(iterations = ENC_ITERATIONS_MEASURE, time = ENC_TIME_MEASURE)
 open class Base32HexBenchmark: EncoderDecoderBenchmarkBase() {
     // CHARS: 0123456789ABCDEFGHIJKLMNOPQRSTUV
-    @Param("A3B4CC2A:$TIME_QUICK", "V7RS4JM6:$TIME_QUICK", "A3B4CC2A:$TIME_CONST", "V7RS4JM6:$TIME_CONST")
-    override var params: String = "<Characters>:<isConstantTime>"
+    @Param("A:-12:$TIME_QUICK", "r:42:$TIME_QUICK", "A:-12:$TIME_CONST", "r:42:$TIME_CONST")
+    override var params: String = "<Char>:<Byte>:<isConstantTime>"
     override fun encoder(isConstantTime: Boolean): EncoderDecoder<*> {
         return Base32Hex { this.isConstantTime = isConstantTime }
     }
@@ -138,8 +118,8 @@ open class Base32HexBenchmark: EncoderDecoderBenchmarkBase() {
 @Measurement(iterations = ENC_ITERATIONS_MEASURE, time = ENC_TIME_MEASURE)
 open class Base64Benchmark: EncoderDecoderBenchmarkBase() {
     // CHARS: ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789
-    @Param("0CaI:$TIME_QUICK", "9tvw:$TIME_QUICK", "0CaI:$TIME_CONST", "9tvw:$TIME_CONST")
-    override var params: String = "<Characters>:<isConstantTime>"
+    @Param("2:84:$TIME_QUICK", "w:22:$TIME_QUICK", "2:84:$TIME_CONST", "w:22:$TIME_CONST")
+    override var params: String = "<Char>:<Byte>:<isConstantTime>"
     override fun encoder(isConstantTime: Boolean): EncoderDecoder<*> {
         return Base64 { this.isConstantTime = isConstantTime }
     }
