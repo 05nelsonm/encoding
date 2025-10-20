430bc7c449ec6d0cd21acc3e3c4191c78fce036c
Matthew Nelson <developer@matthewnelson.io>
2025-10-20 03:58:43 -0400

    Add benchmark for DecoderInput

--- benchmarks/src/commonMain/kotlin/io/matthewnelson/encoding/benchmarks/DecoderInputBenchmark.kt
+++ benchmarks/src/commonMain/kotlin/io/matthewnelson/encoding/benchmarks/DecoderInputBenchmark.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2024 Matthew Nelson
+ * Copyright (c) 2025 Matthew Nelson
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -17,22 +17,29 @@
 
 package io.matthewnelson.encoding.benchmarks
 
-import io.matthewnelson.encoding.core.util.FeedBuffer
-import kotlinx.benchmark.*
+import io.matthewnelson.encoding.core.util.DecoderInput
+import kotlinx.benchmark.Benchmark
+import kotlinx.benchmark.BenchmarkMode
+import kotlinx.benchmark.BenchmarkTimeUnit
+import kotlinx.benchmark.Measurement
+import kotlinx.benchmark.Mode
+import kotlinx.benchmark.OutputTimeUnit
+import kotlinx.benchmark.Scope
+import kotlinx.benchmark.State
+import kotlinx.benchmark.Warmup
 
 @State(Scope.Benchmark)
 @BenchmarkMode(Mode.AverageTime)
 @OutputTimeUnit(BenchmarkTimeUnit.NANOSECONDS)
 @Warmup(iterations = ITERATIONS_WARMUP, time = TIME_WARMUP)
 @Measurement(iterations = ITERATIONS_MEASURE, time = TIME_MEASURE)
-open class FeedBufferBenchmark {
+open class DecoderInputBenchmark {
 
-    private val buffer = object : FeedBuffer(
-        blockSize = 10,
-        flush = Flush { _ -> },
-        finalize = Finalize { _, _ -> }
-    ) {}
+    private val array = CharArray(50)
+    private val input = DecoderInput(array)
 
     @Benchmark
-    fun update() { buffer.update(42) }
+    fun get() {
+        for (i in 0 until array.size) { input[i] }
+    }
 }
