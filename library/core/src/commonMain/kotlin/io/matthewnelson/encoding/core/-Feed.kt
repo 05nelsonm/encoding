50a1a1ad568f2202ce0cb9d8c29b346fd751f88a
Matthew Nelson <developer@matthewnelson.io>
2025-11-03 19:44:21 -0500

    Move `Feed.use` finally logic to internal function instead of inlining it (#189)

--- library/core/src/commonMain/kotlin/io/matthewnelson/encoding/core/-Feed.kt
+++ library/core/src/commonMain/kotlin/io/matthewnelson/encoding/core/-Feed.kt
@@ -13,8 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  **/
-@file:Suppress("SpellCheckingInspection")
-
 package io.matthewnelson.encoding.core
 
 import kotlin.contracts.ExperimentalContracts
@@ -23,30 +21,30 @@ import kotlin.contracts.contract
 import io.matthewnelson.encoding.core.EncoderDecoder.Feed
 
 /**
- * Executes the given [block] function and then closes
- * the [Feed] by either:
- *  - Calling [Feed.doFinal] if [block] **DID NOT** throw an
- *    exception and the feed is still open.
- *  - Calling [Feed.close] if [block] **DID** throw an
- *    exception.
+ * Executes the given [block] function and then closes the [Feed] by either:
+ * - Calling [Feed.doFinal] if [block] **DID NOT** throw an
+ *   exception and the feed is still open.
+ * - Calling [Feed.close] if [block] **DID** throw an exception.
  * */
 @OptIn(ExperimentalContracts::class)
 public inline fun <C: EncoderDecoder.Config, T: Feed<C>?, V> T.use(block: (T) -> V): V {
-    contract {
-        callsInPlace(block, InvocationKind.EXACTLY_ONCE)
-    }
+    contract { callsInPlace(block, InvocationKind.EXACTLY_ONCE) }
 
-    var threw = false
+    var threw: Throwable? = null
     return try {
-        block.invoke(this)
+        block(this)
     } catch (t: Throwable) {
-        threw = true
+        threw = t
         throw t
     } finally {
-        when {
-            this == null -> {}
-            !threw && !isClosed() -> doFinal()
-            else -> close()
-        }
+        useFinally(threw)
     }
+}
+
+@PublishedApi
+@Throws(EncodingException::class)
+internal fun Feed<*>?.useFinally(threw: Throwable?) {
+    if (this == null) return
+    if (isClosed()) return
+    if (threw != null) close() else doFinal()
 }
