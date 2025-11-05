53c4d272228222c1528a80655dd6bb440ee4b2ec
Matthew Nelson <developer@matthewnelson.io>
2025-11-05 15:28:54 -0500

    Add benchmark for UTF8.CharPreProcessor

---
+++ benchmarks/src/commonMain/kotlin/io/matthewnelson/encoding/benchmarks/UTF8CharPreProcessorBenchmark.kt
@@ -0,0 +1,43 @@
+/*
+ * Copyright (c) 2025 Matthew Nelson
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ **/
+@file:Suppress("UNUSED")
+
+package io.matthewnelson.encoding.benchmarks
+
+import io.matthewnelson.encoding.utf8.UTF8
+import kotlinx.benchmark.Benchmark
+import kotlinx.benchmark.BenchmarkMode
+import kotlinx.benchmark.BenchmarkTimeUnit
+import kotlinx.benchmark.Measurement
+import kotlinx.benchmark.Mode
+import kotlinx.benchmark.OutputTimeUnit
+import kotlinx.benchmark.Scope
+import kotlinx.benchmark.State
+import kotlinx.benchmark.Warmup
+
+@State(Scope.Benchmark)
+@BenchmarkMode(Mode.AverageTime)
+@OutputTimeUnit(BenchmarkTimeUnit.NANOSECONDS)
+@Warmup(iterations = ITERATIONS_WARMUP, time = TIME_WARMUP)
+@Measurement(iterations = ITERATIONS_MEASURE, time = TIME_MEASURE)
+open class UTF8CharPreProcessorBenchmark {
+
+    private val cpp = UTF8.CharPreProcessor.of(UTF8)
+    private val char = (Byte.MAX_VALUE.toInt() + 1_000).toChar()
+
+    @Benchmark
+    fun plus() { cpp + char }
+}
